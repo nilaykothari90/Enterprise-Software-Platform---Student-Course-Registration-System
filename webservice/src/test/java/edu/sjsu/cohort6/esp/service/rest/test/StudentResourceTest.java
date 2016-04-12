67d9e16dce594a63b4cf3ef2bd12a983bf698b76
nilaykothari90 <nilay16@gmail.com>
2016-04-11 18:16:00 -0700

    test
    test

--- webservice/src/test/java/edu/sjsu/cohort6/esp/service/rest/test/StudentResourceTest.java
+++ webservice/src/test/java/edu/sjsu/cohort6/esp/service/rest/test/StudentResourceTest.java
@@ -11,60 +11,121 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  */
-
 package edu.sjsu.cohort6.esp.service.rest.test;
 
+import edu.sjsu.cohort6.esp.common.CommonUtils;
+import edu.sjsu.cohort6.esp.common.Course;
+import edu.sjsu.cohort6.esp.common.Student;
+import edu.sjsu.cohort6.esp.common.User;
+import edu.sjsu.cohort6.esp.db.test.DBTest;
+import edu.sjsu.cohort6.esp.service.rest.EndpointUtils;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Created by rwatsh on 9/17/15.
- *
+ * <p>
  * Refer - https://jersey.java.net/documentation/latest/client.html
- *
+ * <p>
  * for more on the Jersey client APIs.
  */
-public class StudentResourceTest {
+public class StudentResourceTest extends BaseResourceTest {
 
-    public static final String BASE_URI = "http://localhost:8080";
-    private Client client;
-    private WebTarget webTarget;
+    public static final String STUDENT_RESOURCE_URI = EndpointUtils.ENDPOINT_ROOT + "/students";
     private static final Logger log = Logger.getLogger(StudentResourceTest.class.getName());
 
-    @BeforeClass
-    public void setUp() throws Exception {
-        client = ClientBuilder.newClient();
-        webTarget = client.target(BASE_URI);
-
+    @Test
+    public void testFetchAll() throws Exception {
+        List<Student> studentList  = getStudents();
+        Assert.assertNotNull(studentList);
     }
 
-    @AfterClass
-    public void tearDown() throws Exception {
-        if (client != null) {
-            client.close();
-        }
-    }
-
-    @org.testng.annotations.Test
-    public void testGetStudent() throws Exception {
-        Response response = webTarget.path("/students")
+    private List<Student> getStudents() throws Exception {
+        Response response = webTarget.path(STUDENT_RESOURCE_URI)
                 .request(MediaType.APPLICATION_JSON_TYPE)
                 .get();
-        logResponse(response);
-        Assert.assertTrue(response.getStatus() == 200);
+        log.info(response.toString());
+        String respStr = response.readEntity(String.class);
+        List<Student> studentList = CommonUtils.convertJsonArrayToList(respStr, Student.class);
+        log.info(studentList.toString());
+        Assert.assertTrue(response.getStatus() == Response.Status.OK.getStatusCode());
+        return studentList;
     }
 
-    private void logResponse(Response response) {
-        log.info(response.toString());
-        log.info(response.readEntity(String.class));
+    @Test
+    public void testFetch() throws Exception {
+        List<Student> studentList = getStudents();
+        if (studentList != null && !studentList.isEmpty()) {
+            String id = studentList.get(0).getId();
+
+            Response response = webTarget.path(STUDENT_RESOURCE_URI)
+                    .queryParam("id", id)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get();
+            log.info(response.toString());
+            String respStr = response.readEntity(String.class);
+            studentList = CommonUtils.convertJsonArrayToList(respStr, Student.class);
+            log.info(studentList.toString());
+            Assert.assertTrue(response.getStatus() == Response.Status.OK.getStatusCode());
+        }
+
     }
+
+
+    @Test
+    public void testAdd() throws Exception {
+        Student s = createStudent();
+        Assert.assertNotNull(s);
+    }
+
+    @Override
+    public void testRemove() throws Exception {
+
+    }
+
+    @Override
+    public void testUpdate() throws Exception {
+
+    }
+
+    private Student createStudent() throws Exception {
+        Student s = new Student();
+        User user = DBTest.getTestUser();
+        s.setUser(user);
+        Course c = DBTest.getTestCourse();
+        List<Course> courses = new ArrayList<>();
+        courses.add(c);
+
+        s.setCourseRefs(courses);
+
+        // Convert to string
+        String studentJson = CommonUtils.convertObjectToJson(s);
+        log.info(studentJson);
+
+        // Use json string to web service.
+
+        Invocation.Builder invocationBuilder = webTarget.path(STUDENT_RESOURCE_URI)
+                .request().accept(MediaType.APPLICATION_JSON_TYPE).header("content-type", MediaType.APPLICATION_JSON);
+
+        Response response = invocationBuilder.post(Entity.json(studentJson));
+
+        log.info(response.toString());
+        //String studentStr = response.readEntity(String.class);
+
+        Student student = CommonUtils.convertJsonToObject(response, Student.class);
+        log.info(s.toString());
+        Assert.assertTrue(response.getStatus() == Response.Status.CREATED.getStatusCode());
+        return s;
+    }
+
+
 
 }
